9b6eebb85892183b273dd833af8351d81fc16877
midorineko <steven.inouye91@gmail.com>
2016-02-29 20:21:42 -0800

    Added angular to cannabis portion of blog

--- _site/script.js
+++ _site/script.js
@@ -1,49 +1,49 @@
-var app = angular.module('myApp', ['ngRoute']);
+	var app = angular.module('myApp', ['ngRoute']);
 
 
-app.config(function($routeProvider) {
-    $routeProvider
+	app.config(function($routeProvider) {
+		$routeProvider
 
-        .when('/', {
-            templateUrl : 'pages/home.html',
-            controller  : 'homeCtrl'
-        })
-        .when('/about', {
-            templateUrl : 'pages/about.html',
-            controller  : 'aboutCtrl'
-        })
-        .when('/contact', {
-            templateUrl : 'pages/contact.html',
-            controller  : 'contactCtrl'
-        })
-        .otherwise('/');
-});
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'homeCtrl'
+			})
+			.when('/kasper', {
+				templateUrl : 'pages/kasper.html',
+				controller  : 'kasperCtrl'
+			})
+		  .when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactCtrl'
+			})
+			.otherwise('/');
+	});
 
-app.controller('mainCtrl', function($scope, $rootScope, $location) {
-
-    $scope.menu = [
-        {label:'Home', route:'/'},
-        {label:'About', route:'/about'},
-        {label:'Contact', route:'/contact'}
-    ]
-
-    $scope.menuActive = '/';
-
-    $rootScope.$on('$routeChangeSuccess', function(e, curr, prev) {
-        $scope.menuActive = $location.path();
+	app.controller('mainCtrl', function($scope, $rootScope, $location) {
+	  
+	  $scope.menu = [
+	    {label:'Home', route:'/'},
+	    {label:'Kasper', route:'/kasper'},
+	    {label:'Contact', route:'/contact'}
+	   ]
+	  
+	  $scope.menuActive = '/';
+	  
+	  $rootScope.$on('$routeChangeSuccess', function(e, curr, prev) {
+       $scope.menuActive = $location.path();
     });
+		
+	});
+	
+	app.controller('homeCtrl', function($scope) {
+		
+		$scope.message = 'Everyone come and see how good I look!';
+	});
 
-});
+	app.controller('kasperCtrl', function($scope) {
+		$scope.message = 'I love Kasper the Ghostie! Soon he will be able to do all this too!';
+	});
 
-app.controller('homeCtrl', function($scope) {
-
-    $scope.message = 'Everyone come and see how good I look!';
-});
-
-app.controller('aboutCtrl', function($scope) {
-    $scope.message = 'Look! I am an about page.';
-});
-
-app.controller('contactCtrl', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
-});
+	app.controller('contactCtrl', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
